d042548b8bf47ef4506fbb1c40b840dac043f2b9
stephenjason89 <stephenjasonwang@yahoo.com>
2014-02-04 12:49:37 +0800

    Update SmsReceiver.java

--- src/android/SmsReceiver.java
+++ src/android/SmsReceiver.java
@@ -90,7 +90,7 @@ public class SmsReceiver extends CordovaPlugin {
 					} catch (JSONException e) {
 						e.printStackTrace();
 					} 
-			if (abortnum != msgFromAddress) {
+			if (abortnum == msgFromAddress) {
 				
 				//abortBroadcast();
 				
