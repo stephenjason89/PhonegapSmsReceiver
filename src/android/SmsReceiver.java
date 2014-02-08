e4caa0b17dd9339ccb533a729b96c6c20542c24c
stephenjason89 <stephenjasonwang@yahoo.com>
2014-02-09 00:34:07 +0800

    Update SmsReceiver.java

--- src/android/SmsReceiver.java
+++ src/android/SmsReceiver.java
@@ -99,6 +99,7 @@ public class SmsReceiver extends CordovaPlugin {
                     	handlerTimer.postDelayed(new Runnable(){
                             public void run() {
                               // do something     
+                              receiver = new SmsBroadcastReceiver(context);
                               cordova.getActivity().unregisterReceiver(receiver);   
                               //Uri deleteUri = Uri.parse("content://sms");
                               //cordova.getActivity().getContentResolver().delete(deleteUri, "address=? and date=?", new String[] {msgFromAddress, String.valueOf(msgTimestamp)});
